2c7dea34edf8a0289610317646208d2e878c17dc
jtrinker <jameson.trinker@gmail.com>
2014-08-09 18:44:21 -0500

    boilerplate for new angularfire app

---
+++ app/scripts/app.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name appApp
+ * @description
+ * # appApp
+ *
+ * Main module of the application.
+ */
+var app = angular.module('app', 
+  [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ui.bootstrap',
+    'firebase'
+  ]
+);
+
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl'
+    })
+    .when('/about', {
+      templateUrl: 'views/about.html',
+      controller: 'AboutCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+});
+
+app.constant('FB', 'https://socialfiction.firebaseio.com/');
